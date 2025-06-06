8bc3a89819fad476d8ac186ab35c471cfa142510
Jonh Sharvey G. Gupo <johnsharveygupo@gmail.com>
2025-06-06 09:34:46 +0800

    Updated homepage design

--- src/Header.jsx
+++ src/Header.jsx
@@ -23,7 +23,7 @@ function Header() {
     <a
       key={index}
       href={button.Link}
-      className="text-choco small-text hover:text-caramel m-5 hidden transition-all ease-in-out sm:block md:m-6.5"
+      className="text-choco large-text md:medium-text lg:small-text hover:text-caramel m-5 hidden transition-all ease-in-out sm:block"
     >
       {button.ButtonName}
     </a>
@@ -38,13 +38,13 @@ function Header() {
         <div className="grow-1 transition-all ease-in-out">
           <a
             href="#landing-page"
-            className="text-choco small-text hover:text-caramel font-bold transition-all ease-in-out"
+            className="text-choco large-text md:medium-text lg:small-text hover:text-caramel font-bold transition-all ease-in-out"
           >
             PORTFOLIO.
           </a>
         </div>
         {ButtonList}
-        <div className="bg-choco hidden h-5 w-0.5 rounded-full sm:block md:ml-6.5 md:w-0.75 lg:h-6 2xl:h-8"></div>
+        <div className="bg-choco hidden h-5 w-0.5 rounded-full sm:block md:ml-5 md:w-0.75 lg:h-6 2xl:h-8"></div>
         <a
           href="https://www.facebook.com/J.S.Gupo"
           target="_blank"
@@ -52,7 +52,7 @@ function Header() {
           className="ml-5 md:ml-10 lg:ml-13"
         >
           <FontAwesomeIcon
-            className="medium-text text-choco hover:text-caramel cursor-pointer transition-all ease-in-out"
+            className="xl-text lg:medium-text text-choco hover:text-caramel cursor-pointer transition-all ease-in-out"
             icon={faSquareFacebook}
           />
         </a>
@@ -64,7 +64,7 @@ function Header() {
           className="ml-4 md:ml-6 lg:ml-8"
         >
           <FontAwesomeIcon
-            className="medium-text text-choco hover:text-caramel cursor-pointer transition-all ease-in-out"
+            className="xl-text lg:medium-text text-choco hover:text-caramel cursor-pointer transition-all ease-in-out"
             icon={faSquareEnvelope}
           />
         </a>
